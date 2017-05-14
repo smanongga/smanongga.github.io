e1877d524232fee6dd8cfbca2235d0c399c5eab9
S. Manongga <smanongga@gmail.com>
2017-05-15 08:49:51 +1200

    Add a new column called taxonomy_term_data

---
+++ migrations/20170515084345_taxonomy_term_data.js
@@ -0,0 +1,14 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('taxonomy_term_data', function (table) {
+    table.increments('id').primary()
+    table.integer('id_vocabulary').unsigned()
+    table.foreign('id_vocabulary').references('taxonomy_vocabulary.id')
+    table.string('title')
+    table.text('description', 'mediumtext')
+  })
+};
+
+exports.down = function(knex, Promise) {
+ return knex.schema.dropTable('taxonomy')
+};
