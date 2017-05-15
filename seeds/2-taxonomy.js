36e36f5608b078cbb64b23647659f91daf8aee87
S. Manongga <smanongga@gmail.com>
2017-05-15 16:44:13 +1200

    Change taxonomy table to taxonomy_vocabulary

--- seeds/2-taxonomy.js
+++ seeds/2-taxonomy.js
@@ -1,10 +1,10 @@
 
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex('taxonomy').del()
+  return knex('taxonomy_vocabulary').del()
     .then(function () {
       // Inserts seed entries
-      return knex('taxonomy').insert([
+      return knex('taxonomy_vocabulary').insert([
         {id: 1, title: 'Technical Blog', description: ''},
         {id: 2, title: 'Cultural Blog', description: ''}
       ]);
