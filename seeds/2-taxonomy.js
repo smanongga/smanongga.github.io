ff4b3a77f9edd65aa5fa4fa7b0552edc0ac41f75
S. Manongga <smanongga@gmail.com>
2017-05-28 15:13:24 +1200

    Edit knex taxonomy seed and added Drupal as tag

--- seeds/2-taxonomy.js
+++ seeds/2-taxonomy.js
@@ -1,12 +1,13 @@
 
-exports.seed = function(knex, Promise) {
+exports.seed = function (knex, Promise) {
   // Deletes ALL existing entries
   return knex('taxonomy_vocabulary').del()
     .then(function () {
       // Inserts seed entries
       return knex('taxonomy_vocabulary').insert([
         {id: 1, title: 'Technical Blog', description: ''},
-        {id: 2, title: 'Cultural Blog', description: ''}
-      ]);
-    });
-};
+        {id: 2, title: 'Cultural Blog', description: ''},
+        {id: 3, title: 'Drupal', description: ''}
+      ])
+    })
+}
