7e47d11ac95e17084d859a6c26f429fdb7374a45
S. Manongga <smanongga@gmail.com>
2017-05-28 15:47:17 +1200

    Add get route for frontpage

--- routes/index.js
+++ routes/index.js
@@ -15,15 +15,33 @@ var storage = multer.diskStorage({
 })
 var upload = multer({storage: storage})
 
+// SITE STRUCTURE
 
+// Frontpage - URL - /
 router.get('/', function (req, res) {
-  db.getUsers(req.app.get('connection'))
-    .then(function (users) {
-      res.render('index', { users: users })
-    })
-    .catch(function (err) {
-      res.status(500).send('DATABASE ERROR: ' + err.message)
-    })
+  db.getRecentBlogs(req.app.get('connection'))
+  .then((results) => {
+    const viewData = {
+      blog: []
+    }
+    for (var i = 0; i < results.length; i++) {
+      const blogEntry = {
+        title: results[i].title,
+        published_date: dateFormat(results[i].published_date, 'mmmm dd yyyy'),
+        image: results[i].image,
+        summary: results[i].summary,
+        first_name: results[i].first_name,
+        last_name: results[i].last_name,
+        profile_id: results[i].profile_id,
+        tags: results[i].tags,
+        slugs: results[i].slugs}
+      viewData.blog.push(blogEntry)
+    }
+    res.render('index', viewData)
+  })
+  .catch(function (err) {
+    res.status(500).send('DATABASE ERROR: ' + err.message)
+  })
 })
 
 module.exports = router
