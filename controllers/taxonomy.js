85f5fa09c7012893703727f460f326bff6a47049
smanongga <smanongga@gmail.com>
2017-06-30 16:45:03 +1200

    Added controller for taxonomy add

--- controllers/taxonomy.js
+++ controllers/taxonomy.js
@@ -1,3 +1,18 @@
 // Taxonomy controller
 
-var db = require('../db/project')
+var db = require('../db/admin')
+
+exports.add = (req, res) => {
+  const taxonomy = {
+    title: req.title,
+    description: req.description
+  }
+
+  db.addTaxonomy(taxonomy, req.app.get('connection'))
+  .then(results => {
+    res.redirect(`/admin/taxonomy`)
+  })
+  .catch(function (err) {
+    res.status(500).send('DATABASE ERROR: ' + err.message)
+  })
+}
