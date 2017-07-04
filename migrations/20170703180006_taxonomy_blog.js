5711db41e33eee21dce07e95d597ce9d67cc63c2
smanongga <smanongga@gmail.com>
2017-07-04 16:46:47 +1200

    Fix typo to table name

--- migrations/20170703180006_taxonomy_blog.js
+++ migrations/20170703180006_taxonomy_blog.js
@@ -2,7 +2,7 @@ exports.up = function (knex, Promise) {
   return knex.schema.createTable('taxonomy_blog', function (table) {
     table.increments('id').primary()
     table.integer('blog_id').unsigned()
-    table.foreign('blog_id').references('blog.id')
+    table.foreign('blog_id').references('blogs.id')
     table.integer('term_id').unsigned()
     table.foreign('term_id').references('taxonomy_term_data.id')
   })
--- migrations/20170704163827_taxonomy_project.js
+++ migrations/20170704163827_taxonomy_project.js
@@ -2,7 +2,7 @@ exports.up = function (knex, Promise) {
   return knex.schema.createTable('taxonomy_project', function (table) {
     table.increments('id').primary()
     table.integer('project_id').unsigned()
-    table.foreign('project_id').references('project.id')
+    table.foreign('project_id').references('projects.id')
     table.integer('term_id').unsigned()
     table.foreign('term_id').references('taxonomy_term_data.id')
   })
