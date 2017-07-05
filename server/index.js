a2d0ca0d8bcadf40bf9059d7e072a35670510fd6
smanongga <smanongga@gmail.com>
2017-07-05 17:16:35 +1200

    Declare knex connection

--- server/index.js
+++ server/index.js
@@ -1,6 +1,10 @@
-var server = require('./server')
+const createServer = require('./server')
+const environment = process.env.NODE_ENV || 'development'
+const config = require('./knexfile')[environment]
+const connection = require('knex')(config)
+const server = createServer(connection)
 
-var PORT = process.env.PORT || 8080
+const PORT = process.env.PORT || 8080
 
 server.listen(PORT, function () {
   console.log('Listening on port', PORT)
