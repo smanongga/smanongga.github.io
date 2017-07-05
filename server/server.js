96079bd224c2d10ad8165d4b3c4389b221875a30
smanongga <smanongga@gmail.com>
2017-07-05 17:33:06 +1200

    Declare api routes and export db

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const path = require('path')
 const express = require('express')
 const bodyParser = require('body-parser')
-const apiRoutes = require('./routes/api')
+const apiRoutes = require('./api')
 
 const server = express()
 
