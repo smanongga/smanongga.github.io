42d5c4ef6287b7dbb2f52959e0b597927b424c84
smanongga <smanongga@gmail.com>
2017-07-05 17:01:32 +1200

    Add route to blog and homepage

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
+import {connect} from 'react-redux'
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+import {BrowserHistory} from 'react-router'
 
+import Home from './Home'
+import BlogList from './BlogList'
 
-const App = () => (
-  <div className='app-container'>
-  </div>
-)
+class App extends React.Component {
+  render () {
+    return (
+      <Router history={BrowserHistory}>
+        <div className='app'>
+          <Switch>
+            <Route exact path='/' component={Home} />
+            <Route exact path='/blogs' component={BlogList} />
+          </Switch>
+        </div>
+      </Router>
+    )
+  }
+}
 
-export default App
+export default connect(App)
