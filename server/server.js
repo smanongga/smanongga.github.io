b65d1ecf7584c5162f4a48ac054fe40861eab5d0
smanongga <smanongga@gmail.com>
2017-07-05 17:16:59 +1200

    Export server connection

--- server/server.js
+++ server/server.js
@@ -1,10 +1,16 @@
-var path = require('path')
-var express = require('express')
-var bodyParser = require('body-parser')
+const path = require('path')
+const express = require('express')
+const bodyParser = require('body-parser')
+const apiRoutes = require('./routes/api')
 
-var server = express()
+const server = express()
 
 server.use(bodyParser.json())
 server.use(express.static(path.join(__dirname, '../public')))
 
-module.exports = server
+server.use('/api/v1/', apiRoutes)
+
+module.exports = function (db) {
+  server.set('db', db)
+  return server
+}
